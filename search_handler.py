650aa4515a0a097554e5b3ccf734fad35d53f5bc
sheikhabdalhamid <sheikhabdalhamidboutaaa@gmail.com>
2025-01-12 16:55:10 +0100

    Create search_handler.py
    Signed-off-by: sheikhabdalhamid <sheikhabdalhamidboutaaa@gmail.com>

---
+++ search_handler.py
@@ -0,0 +1,23 @@
+import json
+
+# محتوى تجريبي للبحث
+content = [
+    {"title": "فتاوى", "url": "fatwa.html"},
+    {"title": "دروس ومواعظ", "url": "lessons.html"},
+    {"title": "خطب الجمعة", "url": "friday-sermons.html"},
+    {"title": "صوتيات", "url": "audios.html"},
+    {"title": "مرئيات", "url": "videos.html"},
+    {"title": "استفسارتكم", "url": "messages.html"}
+]
+
+def search(query):
+    query = query.lower()
+    results = [item for item in content if query in item['title'].lower()]
+    if results:
+        return json.dumps(results[0])
+    else:
+        return json.dumps({"error": "لا يوجد نتائج للبحث"})
+
+if __name__ == '__main__':
+    query = input("أدخل نص البحث: ")
+    print(search(query))
